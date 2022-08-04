07f0cc00bf9189992aba4e8332c3ee4669f90f8d
CookieGMVN <tuyentrandinh00@gmail.com>
2022-08-05 03:30:13 +0700

    Fix test

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ const client = new CSNClient({ cookie: "eyJpdiI6Ik9kNHV6UVFFdjcreGpkdms0N1ZGdlE9
 
 (async () => {
     const search = await client.search({ name: "Tình Anh" });
-    const links = await client.getAudioUrl({ songUrl: search[0].songUrl });
+    const links = await client.getAudioUrl({ songUrl: "https://chiasenhac.vn/mp3/la-phong-lam/lac-chon-hong-tran-tsv6b55tqkqhhf.html" });
+    console.log(search);
     console.log(links["128"]);
 })();
